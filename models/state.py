c57be0860a78bf6653395935177583a20419ca93
Jumulisa <144101506+Jumulisa@users.noreply.github.com>
2024-05-17 11:33:46 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-"""documentation"""
+"""
+This module contains the State class (Blueprint for creating State objects).
+"""
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """class for state"""
+    """
+    This is the state class
+
+    Attributes:
+        name (str): The name of the state
+    """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.name = State.name
