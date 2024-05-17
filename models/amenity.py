46029953a7ee2d94985af1627be8b136dd5713bc
Jumulisa <144101506+Jumulisa@users.noreply.github.com>
2024-05-17 11:28:58 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-"""doc"""
+"""
+This module contains Amenity class (Blueprint for creating Amenity objects).
+"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """amenity class"""
+    """
+    This is the amenity class
+
+    Attributes:
+        name (str): The name of the amenity
+    """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.name = Amenity.name
